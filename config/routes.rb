9386f6c85fc7c5aae90ce2effd64e74745deada2
machida <machida@kmc.gr.jp>
2019-08-19 19:33:30 +0900

    topicsのrouteを整備

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'topics#index'
+  get 'topics/index'
+  get 'topics/show'
+  get 'topics/new'
+  post 'topics/create'
 end
