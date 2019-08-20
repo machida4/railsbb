52a9de578ba546ad3bfddec8d889a9a71152019b
machida <machida@kmc.gr.jp>
2019-08-20 10:39:40 +0900

    userモデルに認証に必要なcolumnを全部盛り

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable, :timeoutable, :trackable, :omniauthable
   has_many :posts
   has_many :topics, through: :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_012841) do
+ActiveRecord::Schema.define(version: 2019_08_20_013842) do
 
   create_table "posts", force: :cascade do |t|
     t.text "message"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2019_08_20_012841) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
