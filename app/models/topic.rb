bd4179c33290b9eb850c0d4047602b199e83ab23
machida <machida@kmc.gr.jp>
2019-08-21 17:58:09 +0900

    スレッドのタイトルがblankのとき初期値が入るようにした

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,14 @@
 class Topic < ApplicationRecord
   has_many :posts
   has_many :user, through: :posts
+
+  before_save :set_default_title
+
+  private
+
+    def set_default_title
+      if self.title.blank?
+        self.title = 'No title'
+      end
+    end
 end
